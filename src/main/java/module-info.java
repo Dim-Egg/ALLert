7f9212367571feb687c490d7be52beb15baf3380
Dim-Egg <dim_egg@outlook.com>
2023-05-03 15:59:14 +0300

    Initial Commit, test project

---
+++ src/main/java/module-info.java
@@ -0,0 +1,16 @@
+module com.allert.allert {
+    requires javafx.controls;
+    requires javafx.fxml;
+    requires javafx.web;
+
+    requires org.controlsfx.controls;
+    requires com.dlsc.formsfx;
+    requires net.synedra.validatorfx;
+    requires org.kordamp.ikonli.javafx;
+    requires org.kordamp.bootstrapfx.core;
+    requires eu.hansolo.tilesfx;
+    requires com.almasb.fxgl.all;
+
+    opens com.allert.allert to javafx.fxml;
+    exports com.allert.allert;
+}
