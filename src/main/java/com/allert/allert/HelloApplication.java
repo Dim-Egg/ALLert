073b932a00c962f4283a828ec55922e09774c773
Dim-Egg <dim_egg@outlook.com>
2023-05-04 22:43:14 +0300

    Icon Tests

--- src/main/java/com/allert/allert/HelloApplication.java
+++ src/main/java/com/allert/allert/HelloApplication.java
@@ -3,6 +3,7 @@ package com.allert.allert;
 import javafx.application.Application;
 import javafx.fxml.FXMLLoader;
 import javafx.scene.Scene;
+import javafx.scene.image.Image;
 import javafx.stage.Stage;
 
 import java.io.IOException;
@@ -11,8 +12,12 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("hello-view.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 320, 240);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load(), 1200, 800);
+        stage.setTitle("Volunteer");
+
+
+        Image image = new Image("https://raw.githubusercontent.com/Dim-Egg/ALLert/e709813c7db573e9e26d13f88693ee46b5c51121/ALLert.png");
+        stage.getIcons().add(image);
         stage.setScene(scene);
         stage.show();
     }
